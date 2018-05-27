31e875e7a3c9b19460e029465d964defa53257ef
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2018-05-27 09:39:14 +0700

    add prefix for filename

--- vid2frame.py
+++ vid2frame.py
@@ -5,16 +5,20 @@ import sys
 print 'your opencv version : %s' %(cv2.__version__)
 
 if len(sys.argv) < 3 :
-  print 'usage : python vid2frame.py videofile output_dir, exiting'
+  print 'usage : python vid2frame.py videofile output_dir frameskip filename_prefix, exiting'
   print sys.argv
   sys.exit(-1)
 
 frameskip = 0
+filename_prefix = ''
 
 filename = sys.argv[1]
 outdir = sys.argv[2]
-if len(sys.argv) == 4 :
+if len(sys.argv) >= 4 :
   frameskip = int(sys.argv[3])
+
+if len(sys.argv) >= 5 :
+  filename_prefix = sys.argv[4]
 
 vidcap = cv2.VideoCapture(filename)
 count = 0
@@ -23,7 +27,7 @@ success = True
 while success :
   success, image = vidcap.read()
   print 'process frame %d' %count
-  cv2.imwrite("%s/frame%d.jpg" %(outdir,count), image)
+  cv2.imwrite("%s/%s_frame%d.jpg" %(outdir,filename_prefix,count), image)
   for i in range(frameskip) :
     success, image = vidcap.read()
   count += 1+frameskip
